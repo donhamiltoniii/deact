c8f3083bcb96bfc7de30126117d083a93a1ebf17
Don Hamilton III <donhamiltoniii@gmail.com>
2019-11-01 13:41:09 -0400

    Finish feature

--- src/index.js
+++ src/index.js
@@ -2,22 +2,34 @@ const entry = document.querySelector(".app");
 
 // entry.innerHTML = "<h1>Hello World!</h1>";
 
-// const nav = create("nav", "nav nav nav");
 // render(nav, header);
 
-render(create("header", create("h1", "Hello World!")), entry);
+render(
+  create("header", [
+    create("h1", "Hello World!"),
+    create("nav", "nav nav nav")
+  ]),
+  entry
+);
 
 function render(child, parent) {
   parent.append(child);
 }
 
 function create(element, content) {
+  console.log("working");
   const createdElement = document.createElement(element);
 
-  if (content instanceof String) {
+  if (typeof content === "string") {
     createdElement.innerHTML = content;
-  } else {
+  } else if (content instanceof HTMLElement) {
     createdElement.append(content);
+  } else if (content instanceof Array) {
+    content.forEach(function(htmlElement) {
+      createdElement.append(htmlElement);
+    });
+  } else {
+    throw new Error("Not a valid content-type for element");
   }
 
   return createdElement;
